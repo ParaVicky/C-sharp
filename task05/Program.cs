79092544b5adc25b814c10f737df3929dd9d268d
Victoria <allyssa@ya.ru>
2023-08-20 17:59:52 +0300

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,10 @@
+﻿// на вход принимает число N, на выходе показывает все целые числа от -N до N
+// 4 -> -4, -3, -2, -1, 0, 1, 2, 3, 4
+Console.WriteLine("введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count} ,");
+    count++;
+}
