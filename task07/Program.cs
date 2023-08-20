915445bbed4df55e3c6e0110a7d70d39a2587fce
Victoria <allyssa@ya.ru>
2023-08-20 18:12:15 +0300

    task07

---
+++ task07/Program.cs
@@ -0,0 +1,13 @@
+﻿// на вход принимает 3-значное число и на выходе выдает последнюю цифру этого числа
+// 456 -> 6
+// 782 -> 2
+Console.WriteLine("введите 3-значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0) number*=-1;
+if (number>=100 && number <=999)
+{
+    int lastDigit = number%10;
+    Console.WriteLine($"Последняя цифра числа {number} -> {lastDigit}");
+}
+else Console.WriteLine("Введено некорректное число");
+
