e798cd791466789991d578e60bb6d2e6a977c721
Victoria <allyssa@ya.ru>
2023-08-23 19:48:36 +0300

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,34 @@
+﻿// написать программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int number = new Random().Next(10,99+1);
+Console.WriteLine($"случайное число из диапазона 10-99 -> {number}");
+
+// int firstDigit = number/10;
+// int secondDigit = number%10;
+
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"наибольшая цифра числа {number} -> {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"наибольшая цифра числа {number} -> {secondDigit}");
+// }
+
+
+// Console.WriteLine(MaxDigit);
+
+int MaxDigit (int number)
+{
+    int firstDigit = number /10;
+    int secondDigit = number %10;
+    if(firstDigit>secondDigit) return firstDigit;
+    return secondDigit;
+}
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"наибольшая цифра числа {number} -> {maxDigit}");
+
+
