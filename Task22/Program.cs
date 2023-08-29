f09044cb5778f930cc8edac99aff5fc6e930095c
Victoria <allyssa@ya.ru>
2023-08-29 19:20:39 +0300

    task22

---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа на вход получает число N и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+void TableOfSquares (int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 3} -> {count*count, 5}");
+        count++;
+    }
+}
+TableOfSquares(number);
