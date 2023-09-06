f52278f8a73429061d53f1646d0b11380dc2c13f
Victoria <allyssa@ya.ru>
2023-09-06 18:42:05 +0300

    task25_hometask

---
+++ task25_hometask/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentation(int num)
+{
+    int exp = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        exp = exp*numberA;
+    }
+    return exp;
+}
+
+
+int result = Exponentation(numberA);
+Console.WriteLine(Exponentation(numberA));
+
+
