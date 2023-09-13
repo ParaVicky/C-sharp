900a96fb61498e196da0e2517f04023a56871658
Victoria <allyssa@ya.ru>
2023-09-13 19:30:55 +0300

    task42 не работает с 4-значными числами

---
+++ task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int DecimalToBinary(int num)
+{
+    int result = 0;
+    int factor = 1;
+    while (num > 0)
+    {
+        result = result + num % 2 * factor;
+        num /= 2;
+        factor *= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int decToBin = DecimalToBinary(number);
+Console.WriteLine(decToBin);
