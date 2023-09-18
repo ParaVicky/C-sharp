220f73d5251100b3a194ab9417933cd8f799ce2a
Victoria <allyssa@ya.ru>
2023-09-18 20:03:02 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+
+int[,] CreateMaxrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverageOFEachColumn(int[,] matrix)
+{
+    double average = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average += matrix[i, j];
+        }
+        average /= matrix.GetLength(0);
+        Console.Write($"{average:F1}" + "···");
+
+    }
+}
+int[,] array2d = CreateMaxrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2d);
+FindAverageOFEachColumn(array2d);
