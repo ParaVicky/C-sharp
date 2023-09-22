f5b855a38046d60e22c153d9287e011046c2b68c
Victoria <allyssa@ya.ru>
2023-09-22 20:25:27 +0300

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumOfDigitsRec (int num)
+{
+    if (num ==0) return 0;
+    return num % 10 + SumOfDigitsRec (num/10);
+}
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {number} = {SumOfDigitsRec(number)}");
